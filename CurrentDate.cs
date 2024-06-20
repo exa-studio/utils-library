6229cf9e482de628b5f95262f067e56f30c0f911
Kevin Ricardo Palencia Gil <kevingil1910@gmail.com>
2024-06-20 12:54:06 -0600

    feat: New release version
    BREAKING CHANGE: V5

--- CurrentDate.cs
+++ CurrentDate.cs
@@ -3,5 +3,6 @@ namespace utils_library;
 public static class CurrentDate
 {
     public static DateTime GetDateTime() => DateTime.Now;
+    public static DateTime GetDateTimeAndOneHour() => DateTime.Now.AddHours(1);
 
 }
