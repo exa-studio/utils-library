c90d8fd4e948dcc35db9c17e07d7078a41609916
Kevin Ricardo Palencia Gil <kevingil1910@gmail.com>
2024-07-05 16:31:56 -0600

    feat: a really cool new feature

--- CurrentDate.cs
+++ CurrentDate.cs
@@ -3,5 +3,5 @@ namespace utils_library;
 public static class CurrentDate
 {
     public static DateTime GetDateTime() => DateTime.Now;
-    public static DateTime GetDateTimeAndOneHour() => DateTime.Now.AddHours(1);
+    public static DateTime GetDateTimeAndOneHour() => DateTime.Now.AddHours(2);
 }
